bd7a41028b2afc08eb84b26b71a2381c52e98b85
Capril1042 <Caprilewis1042@gmail.com>
2019-03-04 20:00:47 -0500

    init commit

---
+++ src/main/java/com/cjs/shoppingcartback/ShoppingcartbackApplication.java
@@ -0,0 +1,15 @@
+package com.cjs.shoppingcartback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartbackApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ShoppingcartbackApplication.class, args);
+    }
+
+}
